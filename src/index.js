134641ab5b88bb13b88426346b2290d05a533d53
macjabeth <macbethjonathan@gmail.com>
2019-03-06 23:28:49 +0000

    setup action consts & handlers

--- src/index.js
+++ src/index.js
@@ -1,21 +1,18 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import { Provider } from "react-redux";
-import { createStore, applyMiddleware } from "redux";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
-import rootReducer from "./reducers";
-import "./index.css";
+import rootReducer from './reducers';
+import './index.css';
 
-const store = createStore(
-  rootReducer
-  applyMiddleware(thunk, logger)
-);
+const store = createStore(rootReducer, applyMiddleware(thunk, logger));
 
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
