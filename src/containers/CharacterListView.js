cd34e0b9b2ac207d18e1c17733183b0da211e7aa
macjabeth <macbethjonathan@gmail.com>
2019-03-06 23:29:31 +0000

    prefer containers over views, fill in character list data

--- src/containers/CharacterListView.js
+++ src/containers/CharacterListView.js
@@ -1,21 +1,17 @@
-import React from "react";
-import { connect } from "react-redux";
+import React from 'react';
+import { connect } from 'react-redux';
 
-import { CharacterList } from "../components";
-// import actions
+import { CharacterList } from '../components';
+import { fetchSwapi } from '../actions';
 
 class CharacterListView extends React.Component {
-  constructor() {
-    super();
-  }
-
   componentDidMount() {
-    // call our action
+    this.props.fetchSwapi();
   }
 
   render() {
     if (this.props.fetching) {
-      // return something here to indicate that you are fetching data
+      return <h3>Loading...</h3>;
     }
     return (
       <div className="CharactersList_wrapper">
@@ -25,11 +21,12 @@ class CharacterListView extends React.Component {
   }
 }
 
-// our mapStateToProps needs to have two properties inherited from state
-// the characters and the fetching boolean
+const mapStateToProps = state => ({
+  characters: state.characters,
+  fetching: state.fetching
+});
+
 export default connect(
-  null /* mapStateToProps replaces null here */,
-  {
-    /* action creators go here */
-  }
+  mapStateToProps,
+  { fetchSwapi }
 )(CharacterListView);
